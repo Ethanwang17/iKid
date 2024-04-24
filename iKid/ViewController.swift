74c1e5626008f5a7710451f5fa4280951ba25ac6
Ethanwang17 <ejw17@uw.edu>
2024-04-24 15:56:09 -0700

    Initial Commit

---
+++ iKid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Ethan Wang on 4/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
